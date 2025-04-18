d15e5075bbea7928c6b99b07706d2ee46b34ef7d
chahatkesh <ckesharwani4@gmail.com>
2025-04-18 08:57:58 +0530

    Swap Progress and Hero component rendering in Home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+import Progress from "./components/home/Progress.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App />
+      {/* <App /> */}
+      <Progress />
     </Router>
   </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Progress from "../components/home/Progress";
 import Hero from "../components/home/Hero";
+
 const Home = () => {
   return (
     <div>
-      <Progress />
-      {/* <Hero /> */}
+      <Hero />
     </div>
   );
 };
