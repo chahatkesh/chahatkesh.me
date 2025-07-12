ae0937ec9d6b4578d93ca1d0c19cf055bf2d514b
chahatkesh <ckesharwani4@gmail.com>
2025-07-12 16:29:39 +0530

    feat: add FeaturedCarousel component and integrate it into the gallery page

--- src/components/gallery/index.ts
+++ src/components/gallery/index.ts
@@ -1,5 +1,6 @@
 import { GalleryGrid } from "./gallery-grid";
+import { FeaturedCarousel } from "./featured-carousel";
 import type { GalleryItem } from "./gallery-grid";
 
-export { GalleryGrid };
+export { GalleryGrid, FeaturedCarousel };
 export type { GalleryItem };
